9a52aada9f48aa7af2b4b29fd947406d418b0e4e
lulzseq <teoahoo@gmail.com>
2023-01-29 13:50:59 +0800

    Reformat folder

--- index.js
+++
@@ -1,4 +0,0 @@
-import "./src/css/style.css"
-import "./src/js/app.js"
-
-console.log('app worked')
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,44 @@
-const path = require("path");
+const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: "production",
-    entry: "./index.js",
-    output:  {
-        path: path.resolve(__dirname, "src"),
-        filename: "./js/app.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/, 
-                use: [
-                    MiniCssExtractPlugin.loader, 
-                    'css-loader',
-                ],
-            },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
         ],
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: './css/style.css'
-        }),
-
-        new HtmlWebPackPlugin({
-            title: 'Webpack',
-            template: './index.html',
-            filename: './main.html'
-        }),
-    ]
-}
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
